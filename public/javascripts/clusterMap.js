9d3a45dde998964baf9c25488f6107f3a2f9990b
Colt Steele <coltsteele@MacBook-Pro.lan>
2020-09-18 14:21:45 -0700

    basic styling changes

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,10 +1,12 @@
 mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
-    container: 'map',
+    container: 'cluster-map',
     style: 'mapbox://styles/mapbox/light-v10',
     center: [-103.59179687498357, 40.66995747013945],
     zoom: 3
 });
+
+map.addControl(new mapboxgl.NavigationControl());
 
 
 
--- public/javascripts/showPageMap.js
+++ public/javascripts/showPageMap.js
@@ -6,6 +6,9 @@ const map = new mapboxgl.Map({
     zoom: 10 // starting zoom
 });
 
+map.addControl(new mapboxgl.NavigationControl());
+
+
 new mapboxgl.Marker()
     .setLngLat(campground.geometry.coordinates)
     .setPopup(
