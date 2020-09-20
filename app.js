3ef5c4ca6aae9243b28167db3c3fb0665c3ea46a
Colt Steele <coltsteele@MacBook-Pro.lan>
2020-09-19 21:25:43 -0700

    add start script

--- app.js
+++ app.js
@@ -159,8 +159,9 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err })
 })
 
-app.listen(3000, () => {
-    console.log('Serving on port 3000')
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Serving on port ${port}`)
 })
 
 
