5c3aac5c4beed638caa605d5167433d4ebe669e3
Ian <iantskon@gmail.com>
2020-12-23 17:57:20 -0600

    Fix returnTo bug

--- middleware.js
+++ middleware.js
@@ -5,7 +5,6 @@ const Review = require('./models/review');
 
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
-        req.session.returnTo = req.originalUrl
         req.flash('error', 'You must be signed in first!');
         return res.redirect('/login');
     }
