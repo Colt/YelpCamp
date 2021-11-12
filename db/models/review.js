6689bcd37ce5b5aa3f82218739710e1eaddcd21e
Ak <akalankapathirage22@gmail.com>
2021-11-12 14:52:27 +0530

    Added the Review model as many to one relationship with Campground model.

--- db/models/review.js
+++ db/models/review.js
@@ -1,24 +1,13 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-
 // Review model has a one to many relationship with Campground model. Which means every campground
 // doucment has to fit to 16mb threshold. If the reviews array is going to be larger than couple thousands we have to
 // use one to bajjilions relationship.
 
-const CampgroundSchema = new Schema({
-    title: String,
-    image: String,
-    price: Number,
-    description: String,
-    location: String,
-    reviews : [
-        {
-            type : Schema.Types.ObjectId,
-            ref : 'Review'
-        }
-    ]
-});
+const reviewSchema = new Schema({
+    body : String,
+    rating : Number,
+})
 
-
-module.exports = mongoose.model('Campground', CampgroundSchema);
+module.exports = mongoose.model('Review',reviewSchema)
