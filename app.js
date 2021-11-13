db72474e47818a552dc53a322e9b56b397bbecb7
Ak <akalankapathirage22@gmail.com>
2021-11-13 17:03:05 +0530

    set up public directory

--- app.js
+++ app.js
@@ -22,6 +22,8 @@ app.set('views', path.join(__dirname, 'views'))
 // middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+app.use(express.static(path.join(__dirname,'public')))
+
 
 //connect to DB
 connectDB()
