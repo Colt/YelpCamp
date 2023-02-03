ce18b7c609d6db37f00bf432da99921a688336ce
evixedor <evi-edor@Evi.local>
2023-02-03 05:28:36 -0500

    approach 5

--- dist/app.js
+++ dist/app.js
@@ -132,6 +132,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next) => {
+    res.setHeader('Content-Type', '');
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
     res.locals.error = req.flash('error');
