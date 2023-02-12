3011bfbc4d0434cf863171af6181b99373c85596
evixedor <evi-edor@Evi.local>
2023-02-12 06:54:45 -0500

    updating app for render deploy

--- dist/utils/ExpressError.js
+++
@@ -1,9 +0,0 @@
-class ExpressError extends Error {
-    constructor(message, statusCode) {
-        super();
-        this.message = message;
-        this.statusCode = statusCode;
-    }
-}
-
-module.exports = ExpressError;
--- dist/utils/catchAsync.js
+++
@@ -1,5 +0,0 @@
-module.exports = func => {
-    return (req, res, next) => {
-        func(req, res, next).catch(next);
-    }
-}
