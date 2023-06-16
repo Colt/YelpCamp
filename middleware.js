155323036e665aacc49772b8ea50807501513d48
Habibi <134970522+X0Rhyth@users.noreply.github.com>
2023-06-16 15:56:18 -0400

    Update middleware.js
    Checks if the campground exists before checking whether the current user is the author.

--- middleware.js
+++ middleware.js
@@ -1,54 +1,70 @@
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
-const ExpressError = require('./utils/ExpressError');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
+const { sequelize } = require('./database/database');
+const db = require("./database/db.js");
+const {joiCampSchema, joiReviewSchema} = require("./schemas")
+const ExpressError = require('./util/ExpressError');
+const { Campground, Review, User } = require('./models/models');
 
-module.exports.isLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-        req.session.returnTo = req.originalUrl
-        req.flash('error', 'You must be signed in first!');
-        return res.redirect('/login');
-    }
-    next();
+module.exports.isLoggedIn = (req,res,next) => { 
+    if(!req.isAuthenticated()){
+      req.flash("error", "You must be signed in to perform this action");
+      return res.redirect("/login") //the return statemnt is necessary otherwise the code below would still run
+  }
+  next();
 }
 
-module.exports.validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    console.log(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
+module.exports.validateCampground = (req,res,next)=>{
+    const {error} = joiCampSchema.validate(req.body);
+    if(error) {
+      const msg = error.details.map(el => el.message).join(",")
+      throw new ExpressError(msg, 400)
     } else {
         next();
     }
-}
+  }
 
-module.exports.isAuthor = async (req, res, next) => {
+
+  module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
-    const campground = await Campground.findById(id);
-    if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
+    try {
+      const campground = await Campground.findByPk(id);
+      if (!campground) {
+        req.flash("error", "Campground doesn't exist");
         return res.redirect(`/campgrounds/${id}`);
+      }
+      if (campground.authorid !== req.user.id) {
+        req.flash("error", "You don't have permission");
+        return res.redirect(`/campgrounds/${id}`);
+      }
+      next();
+    } catch (error) {
+      next(error);
     }
-    next();
-}
-
-module.exports.isReviewAuthor = async (req, res, next) => {
+  };
+  
+  module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
-    const review = await Review.findById(reviewId);
-    if (!review.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that!');
+    try {
+      const review = await Review.findByPk(reviewId);
+      if (!req.user) {
+        req.flash("error", "You don't have permission");
         return res.redirect(`/campgrounds/${id}`);
+      } else if (review.authorid !== req.user.id) {
+        req.flash("error", "You don't have permission");
+        return res.redirect(`/campgrounds/${id}`);
+      }
+      next();
+    } catch (error) {
+      next(error);
     }
-    next();
-}
+  };
+  
 
-module.exports.validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',')
-        throw new ExpressError(msg, 400)
+  module.exports.validateReview = (req,res,next)=>{
+    const {error} = joiReviewSchema.validate(req.body);
+    if(error) {
+      const msg = error.details.map(el => el.message).join(",")
+      throw new ExpressError(msg, 400)
     } else {
         next();
     }
-}
+  }
